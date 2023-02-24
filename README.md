b268f3ad30b65a336e1bc4a559296d486bb8cf01
angelhdzmultimedia <angel.hernandez.12@live.com>
2023-02-24 04:02:45 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# express-session-test
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-ycluk6)
---
+++ src/main.ts
@@ -0,0 +1,47 @@
+import express, { Request, Response } from 'express'
+import session from 'express-session'
+
+const app = express()
+app.use(express.json())
+app.use(
+  session({
+    secret: 'asdasdasdasdasd',
+    cookie: {
+      maxAge: 20 * 60 * 360,
+    },
+    saveUninitialized: false,
+    resave: false,
+  })
+)
+const router = express.Router()
+
+declare module 'express-session' {
+  interface SessionData {
+    count: number
+  }
+}
+
+router.get('/count/reset', (req: Request, res: Response) => {
+  req.session.count = 1
+  return res.send(`Count: ${req.session.count}`)
+})
+
+router.get('/count/add', (req: Request, res: Response) => {
+  if (!req.session.count) {
+    req.session.count = 1
+  }
+  return res.send(`Count: ${++req.session.count}`)
+})
+
+router.get('/count', (req: Request, res: Response) => {
+  if (!req.session.count) {
+    req.session.count = 1
+  }
+  return res.send(`Count: ${req.session.count}`)
+})
+
+app.use('/api', router)
+
+app.listen(5000, () => {
+  console.log('Server listening on port 5000...')
+})
